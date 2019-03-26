f1a5cfffb01d5863fa0374c6b17e0209cf425181
Bazyli Cyran <bazyli.cyran@gmail.com>
2019-03-26 11:14:30 +0100

    Allow CORS requests in dev mode

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,8 @@
+import os
 from flask import Flask
 from flask_restful import Api
 from flask_jwt_extended import JWTManager
+from flask_cors import CORS
 from backend import config
 from backend.resources import auth
 
@@ -8,6 +10,9 @@ app = Flask(__name__)
 app.config.from_object(config)
 api = Api(app)
 jwt = JWTManager(app)
+
+if os.environ.get('FLASK_ENV') == 'development':
+    CORS(app)
 
 api.add_resource(auth.Auth, '/auth')
 
