d6edd1d93dc53ae07cefb998a7f30eb2b7db07c2
Bazyli Cyran <bazyli.cyran@gmail.com>
2019-03-27 20:09:22 +0100

    Improve db config

--- backend/app.py
+++ backend/app.py
@@ -12,6 +12,9 @@ app.config.from_object(config)
 app.config['JWT_IDENTITY_CLAIM'] = 'sub'
 app.config['JWT_USER_CLAIMS'] = 'user'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://%s:%s@%s:%s/%s' % \
+    (app.config['PG_USER'], app.config['PG_PASS'], app.config['PG_URL'],
+     app.config['PG_PORT'], app.config['PG_DB'])
 
 api = Api(app)
 jwt = JWTManager(app)
