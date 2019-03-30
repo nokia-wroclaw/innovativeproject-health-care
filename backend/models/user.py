cd3eff423ebead71a4ca875f38a90b318e0c8857
Bazyli Cyran <bazyli.cyran@gmail.com>
2019-03-30 17:14:01 +0100

    Add admin list to config

--- backend/models/user.py
+++ backend/models/user.py
@@ -1,4 +1,4 @@
-from backend.app import db
+from backend.app import db, app
 
 
 class User(db.Model):
@@ -22,3 +22,41 @@ class User(db.Model):
         self.mail = mail
         self.full_name = full_name
         self.editor = editor
+
+    @staticmethod
+    def from_ldap(data):
+        user = User.query.filter_by(id=data['id']).first()
+        if user is not None:
+            return user;
+        else:
+            user = User(data['login'], data['mail'], data['name'], False)
+            user.id = data['id']
+            return user
+
+    def is_admin(self):
+        if self.login in app.config['APP_ADMINS']:
+            return True
+        return False
+
+    def is_editor(self):
+        return self.editor
+
+    def is_manager(self):
+        for team in self.teams:
+            if team.manager is True:
+                return True
+        return False
+
+    def is_user(self):
+        for team in self.teams:
+            if team.manager is not True:
+                return True
+        return False
+
+    def get_json(self):
+        return {
+            'id': self.id,
+            'login': self.login,
+            'mail': self.mail,
+            'name': self.full_name
+        }
--- backend/resources/auth.py
+++ backend/resources/auth.py
@@ -2,7 +2,9 @@ from datetime import timedelta
 from flask import request, abort, jsonify
 from flask_restful import Resource
 from flask_jwt_extended import create_access_token
+from backend.app import app
 from backend.common.ldapconn import LdapConn
+from backend.models import User
 
 
 class Auth(Resource):
@@ -20,17 +22,30 @@ class Auth(Resource):
 
         credentials = request.authorization
 
+        # If user didn't send credentials
         if not credentials:
             abort(401)
 
         auth = self.ldap.authenticate(credentials.username,
                                       credentials.password)
 
+        # If credentials are incorrect
         if not auth:
             abort(401)
 
-        user = self.ldap.search(credentials.username, True)[0]
-        token = create_access_token(user['id'], user_claims=user,
+        # Retrive user data from LDAP and create user object
+        ldap_user = self.ldap.search(credentials.username, True)[0]
+        user = User.from_ldap(ldap_user)
+
+        # Try to fetch user id from db
+        exists = User.query.filter_by(id=user.id).first()
+ 
+        # Abort if user is not in db and is not an admin
+        if exists is None:
+            if user.is_admin() is False:
+                abort(401)
+
+        token = create_access_token(user.id, user_claims=user.get_json(),
                                     expires_delta=timedelta(hours=1))
 
         response = jsonify({'access_token': token})
