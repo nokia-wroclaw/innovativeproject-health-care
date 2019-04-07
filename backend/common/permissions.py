a7ed6158cfcc529a3d7c546adee1e034a52d4a58
Bazyli Cyran <bazyli.cyran@gmail.com>
2019-04-07 15:00:46 +0200

    Add editor_required decorator

--- backend/common/permissions.py
+++ backend/common/permissions.py
@@ -21,3 +21,35 @@ def admin_required(fn):
             return abort(403)
 
     return wrapper
+
+
+def editor_required(tribe_id=None):
+    '''Allows function execution if token bearer is an editor or admin.
+    If specific tribe id is given it will allow only if he has rights
+    to this tribe.
+    '''
+
+    def real_editor_required(fn):
+        @wraps(fn)
+        def wrapper(*args, **kwargs):
+            verify_jwt_in_request()
+            id = get_jwt_identity()
+            requesting_user = User.from_id(id)
+            requested_tribe_id = tribe_id if not callable(tribe_id) else None
+
+            if requesting_user.is_admin():
+                return fn(*args, **kwargs)
+
+            if requested_tribe_id is None:
+                if requesting_user.is_editor() is False:
+                    return abort(403)
+            else:
+                if requested_tribe_id not in requesting_user.editing_ids():
+                    return abort(403)
+
+            return fn(*args, **kwargs)
+        return wrapper
+
+    if callable(tribe_id):
+        return real_editor_required(tribe_id)
+    return real_editor_required
