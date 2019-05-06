19fc5835ac0c09cb9f43b6dbe17c6c3f1a9cdd73
Paweł Komorowski <pkomorowski8@gmail.com>
2019-05-06 22:00:34 +0200

    Add POST request to send answers

--- backend/resources/surveys.py
+++ backend/resources/surveys.py
@@ -5,7 +5,7 @@ from flask_jwt_extended import current_user
 from sqlalchemy import exc
 from backend.common.permissions import roles_allowed
 from backend.app import db
-from backend.models import Tribe, Survey, Question, SurveyQuestionLink
+from backend.models import Tribe, Survey, Question, SurveyQuestionLink, Answer
 
 
 class TribeSurveysRes(Resource):
@@ -112,4 +112,77 @@ class SurveyRes(Resource):
 
         response = jsonify(survey.serialize())
         response.status_code = 200
+        return response
+
+
+class SurveyAnswersRes(Resource):
+    """Survey answer"""
+
+    @roles_allowed(['user'])
+    def post(self, survey_id):
+        """Save survey answers"""
+
+        survey = Survey.query.filter_by(id=survey_id, draft=False).one_or_none()
+
+        # There is nor survey or it is not published yet
+        if survey is None:
+            abort(404, 'Requested survey does not exist.')
+
+        # User have to be in team which is in tribe which has that survey to send answers
+        access = False
+        if current_user.is_user():
+            tribe_ids = [t.team.tribe_id for t in current_user.teams
+                         if t.manager if False]
+            if survey.tribe_id in tribe_ids:
+                access = True
+
+        if access is False:
+            abort(403)
+
+        # Answers data
+        answers = request.get_json()
+        # Answers ids
+        answers_ids = [a['question_id'] for a in answers]
+        # Questions data
+        questions = survey.serialize_questions()
+        # Questions ids
+        questions_ids = [q['id'] for q in questions]
+
+        # Check if there are questions for all answers
+        # and if there are answers for all questions
+        # and if all necessary comments was put
+        for a in answers:
+                # Answers for all questions
+                if a['question_id'] not in questions_ids:
+                    abort(400, 'Invalid answer data.')
+                # Comment section
+                if 'comment' not in a:
+                    abort(400, 'Invalid answer data.')
+                # Necessary comments
+                if int(a['answer']) < 2 and len(a['comment'] == 0):
+                    abort(400, 'Invalid answer data.')
+
+        # Questions for all answers
+        for q in questions_ids:
+            if q not in answers_ids:
+                abort(400, 'Invalid answer data.')
+
+        # All fine
+        # Answer objects list
+        answer_ojb = []
+        now = datetime.now()
+        for a in answers:
+            answer = Answer(a['question_id'], now, a['answer'], a['comment'])
+            db.session.add(answer)
+            answer_ojb.append(answer)
+        db.session.commit()
+
+        answer_json = []
+        for a in answer_ojb:
+            answer_json.append(a.serialize())
+
+        response = jsonify(answer_json)
+        response.headers['Location'] = '/surveys/%d/answers' % survey_id
+        response.status_code = 201
+
         return response
