20307dfcc3b3be56d5ebe573b75eaab22db6479b
Bazyli Cyran <bazyli.cyran@gmail.com>
2019-05-09 17:34:48 +0200

    Update docker configs

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -11,7 +11,9 @@ const store = createStore(
   initialState,
   compose(
     applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    process.env.NODE_ENV !== 'production' ?
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() :
+        f => f
   )
 );
 
