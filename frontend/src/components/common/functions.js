c84a5fddb75aa2cea0bbc3a2e4eab98b417ad17a
Agata <agatatocz@gmail.com>
2019-06-10 16:38:13 +0200

    improved fetching users data list

--- frontend/src/components/common/functions.js
+++ frontend/src/components/common/functions.js
@@ -9,16 +9,3 @@ export const confirmDelete = (itemName, customMessage) => {
 export const isAsync = fn => {
   return fn.constructor.name === "AsyncFunction";
 };
-
-export const replaceNotLettersWithSpace = text => {
-  const notLetters = /[^A-Za-z]/g;
-  return text.replace(notLetters, " ");
-};
-
-export const replaceSpaceWithUnderscore = text => {
-  return text.replace(/\s/g, "_");
-};
-
-export const removeSpaceAtBegening = text => {
-  return text.replace(/^\s+/g, "");
-};
