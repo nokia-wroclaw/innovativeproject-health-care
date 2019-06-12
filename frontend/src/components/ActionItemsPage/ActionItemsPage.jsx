407ca75c1d47ec523d7e886b3e3fa22f3699118b
Bazyli Cyran <bazyli.cyran@gmail.com>
2019-06-12 18:36:47 +0200

    Add 'not implemented' message to the 'action items' view

--- frontend/src/components/ActionItemsPage/ActionItemsPage.jsx
+++ frontend/src/components/ActionItemsPage/ActionItemsPage.jsx
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import TemplatePage from '../common/TemplatePage/';
+import { Container, Message } from "semantic-ui-react";
 
 class ActionItemsPage extends Component {
   render() {
     return (
       <TemplatePage>
-        <h3>Action items page</h3>
+        <Container>
+          <br/>
+          <Message>Sorry, this feature is not yet implemented.</Message>
+        </Container>
       </TemplatePage>
     );
   }
